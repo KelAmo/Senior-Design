b01ff17068695fd3187f00b1debc3aa6c5a40ecd
Kellie Amott <kellamott@gmail.com>
2014-09-02 14:41:55 -0500

    Create HelloWorld.cpp
    Basic hello world program in C++.

---
+++ HelloWorld.cpp
@@ -0,0 +1,15 @@
+// To compile type:
+// g++ -o hello HelloWorld.cpp 
+// in a command prompt in the directory where this file exists
+// Next, type:
+// hello
+// in the same directory where an executable entitled "hello" 
+// has been created
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!What's Up World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+} 
